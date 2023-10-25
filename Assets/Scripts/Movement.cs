0b5793d07aec7b2665a7d4fbd30295ec876710f5
asrk2698 <90810511+asrk2698@users.noreply.github.com>
2023-10-25 10:36:42 +0800

    Initial Commit

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private SpriteRenderer sprite;
+
+    private float dirX = 0f;
+    [SerializeField] private float moveSpeed = 7f;
+    [SerializeField] private float jumpForce = 14f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        dirX = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(dirX * moveSpeed, rb.velocity.y);
+
+        if(Input.GetButtonDown("Jump"))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+        if(dirX > 0)
+        {
+            sprite.flipX = false;
+        }
+        else if (dirX > 0)
+        {
+            sprite.flipX = true;
+        }
+    }
+}
