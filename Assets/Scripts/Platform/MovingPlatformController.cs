6e7edaab4a6a910e7d98abd2f6aa817643314970
Stygian84 <nicholasgandhi84@gmail.com>
2023-11-05 16:51:02 +0800

    Added MovingPlatform Prefab

---
+++ Assets/Scripts/Platform/MovingPlatformController.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatformController : MonoBehaviour
+{
+    public GameObject[] waypoints;
+    private int currentWaypointIndex = 0;
+
+    public float speed = 2f;
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (
+            Vector2.Distance(waypoints[currentWaypointIndex].transform.position, transform.position)
+            < .1f
+        )
+        {
+            currentWaypointIndex++;
+            if (currentWaypointIndex >= waypoints.Length)
+            {
+                currentWaypointIndex = 0;
+            }
+        }
+        transform.position = Vector2.MoveTowards(
+            transform.position,
+            waypoints[currentWaypointIndex].transform.position,
+            Time.deltaTime * speed
+        );
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "Player")
+        {
+            collision.gameObject.transform.SetParent(transform);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "Player")
+        {
+            collision.gameObject.transform.SetParent(null);
+        }
+    }
+
+    public void ResetPlatform()
+    {
+        this.transform.localPosition = new Vector3(0, 0, 0);
+    }
+}
