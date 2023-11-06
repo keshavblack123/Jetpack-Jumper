47f874e338eaeb8b764e5d736a5eee2757757288
Stygian84 <nicholasgandhi84@gmail.com>
2023-11-06 15:49:58 +0800

    Smoother Camera Movement using DoTween

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,10 +1,13 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class CameraController : MonoBehaviour
 {
     private Transform player;
+
+    private Vector3 offset = new Vector3(0f, 0f, -10f);
+    public float smoothTime = 0.125f;
+    private Vector3 velocity = Vector3.zero;
 
     // Start is called before the first frame update
     void Start()
@@ -15,12 +18,34 @@ public class CameraController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector3(
-            player.position.x,
-            player.position.y,
-            transform.position.z
-        );
+        Vector3 targetPosition = player.position + offset;
+        transform.DOMove(targetPosition, smoothTime).SetEase(Ease.InOutQuad);
     }
 
     public void ResetCamera() { }
+
+    // WithoutDoTween
+    // private Transform player;
+
+    // private Vector3 offset = new Vector3(0f, 0f, -10f);
+    // public float smoothTime = 0.125f;
+    // private Vector3 velocity = Vector3.zero;
+
+    // // Start is called before the first frame update
+    // void Start()
+    // {
+    //     player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    // }
+
+    // // Update is called once per frame
+    // void Update()
+    // {
+    //     Vector3 targetPosition = player.position + offset;
+    // transform.position = Vector3.SmoothDamp(
+    //     transform.position,
+    //     targetPosition,
+    //     ref velocity,
+    //     smoothTime
+    // );
+    // }
 }
