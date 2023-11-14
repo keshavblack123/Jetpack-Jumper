cdb4a132528a1a856c42ea3c659065500c711391
asrk2698 <90810511+asrk2698@users.noreply.github.com>
2023-11-14 15:55:31 +0800

    Update CameraController.cs

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -3,6 +3,28 @@ using DG.Tweening;
 
 public class CameraController : MonoBehaviour
 {
+    //private Transform player;
+
+    //private Vector3 offset = new Vector3(0f, 0f, -10f);
+    //public float smoothTime = 0.125f;
+    //private Vector3 velocity = Vector3.zero;
+
+    //// Start is called before the first frame update
+    //void Start()
+    //{
+    //    player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    //}
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+    //    Vector3 targetPosition = player.position + offset;
+    //    transform.DOMove(targetPosition, smoothTime).SetEase(Ease.InOutQuad);
+    //}
+
+    //public void ResetCamera() { }
+
+    // WithoutDoTween
     private Transform player;
 
     private Vector3 offset = new Vector3(0f, 0f, -10f);
@@ -19,33 +41,11 @@ public class CameraController : MonoBehaviour
     void Update()
     {
         Vector3 targetPosition = player.position + offset;
-        transform.DOMove(targetPosition, smoothTime).SetEase(Ease.InOutQuad);
+        transform.position = Vector3.SmoothDamp(
+            transform.position,
+            targetPosition,
+            ref velocity,
+            smoothTime
+        );
     }
-
-    public void ResetCamera() { }
-
-    // WithoutDoTween
-    // private Transform player;
-
-    // private Vector3 offset = new Vector3(0f, 0f, -10f);
-    // public float smoothTime = 0.125f;
-    // private Vector3 velocity = Vector3.zero;
-
-    // // Start is called before the first frame update
-    // void Start()
-    // {
-    //     player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
-    // }
-
-    // // Update is called once per frame
-    // void Update()
-    // {
-    //     Vector3 targetPosition = player.position + offset;
-    // transform.position = Vector3.SmoothDamp(
-    //     transform.position,
-    //     targetPosition,
-    //     ref velocity,
-    //     smoothTime
-    // );
-    // }
 }
