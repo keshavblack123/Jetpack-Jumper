adb6c8072d26855129e6e8a5fda52e97886ed4d5
Stygian84 <nicholasgandhi84@gmail.com>
2023-11-28 19:29:15 +0800

    Fix Sticky Platform

--- Assets/Scripts/Platform/MovingPlatformController.cs
+++ Assets/Scripts/Platform/MovingPlatformController.cs
@@ -34,7 +34,7 @@ public class MovingPlatformController : MonoBehaviour
         );
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerStay2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
